f833e2791239dda42d0091735c9212846c728ba3
Cool Coder � <myfake@email.com>
2025-11-04 13:14:50 +0530

    Update nodemailer3

--- config/nodemailer.js
+++ config/nodemailer.js
@@ -1,16 +1,11 @@
 // config/nodemailer.js
 import nodemailer from 'nodemailer';
 
+// Extracted from admission.js
 export const transporter = nodemailer.createTransport({
-    host: 'smtp.gmail.com',
-    port: 587,         // <-- CHANGED PORT
-    secure: false,     // <-- MUST be false for port 587 (uses STARTTLS)
-    connectionTimeout: 60000, 
-    socketTimeout: 60000,
-    logger: true, 
-    debug: true,  
+    service: 'gmail',
     auth: { 
         user: process.env.EMAIL_USER, 
-        pass: process.env.EMAIL_PASS // App Password still required
+        pass: process.env.EMAIL_PASS 
     }
 });

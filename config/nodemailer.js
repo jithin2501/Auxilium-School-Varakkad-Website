703ee36c25af1eac6ea71cd68bf1f5f31c8d15d7
Cool Coder � <myfake@email.com>
2025-12-11 18:06:25 +0530

    nodemailer

--- config/nodemailer.js
+++ config/nodemailer.js
@@ -3,8 +3,8 @@ import nodemailer from "nodemailer";
 
 export const transporter = nodemailer.createTransport({
   host: "smtp-relay.brevo.com",
-  port: 587,
-  secure: false,
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.BREVO_SMTP_USER,
     pass: process.env.BREVO_SMTP_KEY,
